e1e0c59c3eeef88321be94a3d38e825cc3c187dc
Remzi Gunes <cemr_1919@hotmail.com>
2019-10-14 11:29:16 +0300

    showResult

--- src/Routes.js
+++ src/Routes.js
@@ -5,11 +5,15 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 
 //Screen
 import SearchScreen from './screens/SearchScreen';
+import ResultShowScreen from './screens/ResultShowScreen';
 
 const MainStack = createStackNavigator(
   {
     Search: {
       screen: SearchScreen,
+    },
+    ResultShow: {
+      screen: ResultShowScreen,
     },
   },
   {
---
+++ src/screens/ResultShowScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const ResultShowScreen = () => {
+  return (
+    <View>
+      <Text>ResultShowScreen</Text>
+    </View>
+  );
+};
+
+export default ResultShowScreen;
+
+const styles = StyleSheet.create({});
