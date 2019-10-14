fe81fe881ad3481880a7aeb1825eae03c9e0e34a
Remzi Gunes <cemr_1919@hotmail.com>
2019-10-14 10:57:39 +0300

    showResult

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, TextInput} from 'react-native';
+import {View, Text, StyleSheet, TextInput, ScrollView} from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
 import useResults from '../hooks/useResults';
@@ -19,7 +19,7 @@ const SearchScreen = () => {
   };
 
   return (
-    <View style={{marginLeft: 15}}>
+    <View style={{marginLeft: 15, flex: 1}}>
       <SearchBar
         term={term}
         onTermChange={newTerm => setTerm(newTerm)}
@@ -27,9 +27,17 @@ const SearchScreen = () => {
       />
       {error ? <Text>{error}</Text> : null}
       <Text>we have found {results.length} results </Text>
-      <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
-      <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
-      <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
+      <ScrollView>
+        <ResultsList
+          results={filterResultsByPrice('$')}
+          title="Cost Effective"
+        />
+        <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+        <ResultsList
+          results={filterResultsByPrice('$$$')}
+          title="Big Spender"
+        />
+      </ScrollView>
     </View>
   );
 };
