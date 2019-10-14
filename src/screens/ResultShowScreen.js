300a64c7d902fcd45fffea307e7a5a6ffaa94f5b
Remzi Gunes <cemr_1919@hotmail.com>
2019-10-14 12:03:44 +0300

    navigation

--- src/screens/ResultShowScreen.js
+++ src/screens/ResultShowScreen.js
@@ -1,16 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import yelp from '../api/yelp';
 
 const ResultShowScreen = ({navigation}) => {
   const [result, setResult] = useState(null);
   const id = navigation.getParam('id'); //resultList te olan id dir
-  //console.log(id);
+  console.log(id);
+  // console.log(result);
   const getResult = async id => {
-    const response = await yelp.get(`${id}`);
+    const response = await yelp.get(`/${id}`);
 
-    response.data;
+    setResult(response.data);
   };
+  useEffect(() => {
+    getResult(id);
+  }, []);
   return (
     <View>
       <Text>ResultShowScreen</Text>
