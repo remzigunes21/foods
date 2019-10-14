f18ad6d7937ceb43f867882d4a6e52f4eaa6bbf0
Remzi Gunes <cemr_1919@hotmail.com>
2019-10-14 10:28:36 +0300

    showResult

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const ResultsDetail = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default ResultsDetail;
+
+const styles = StyleSheet.create({});
